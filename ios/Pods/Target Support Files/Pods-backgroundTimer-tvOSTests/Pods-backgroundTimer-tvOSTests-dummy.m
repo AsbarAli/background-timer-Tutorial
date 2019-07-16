e75a217fe261d3ce846101cf9dfaac196bec4a8e
AsbarAli <loons.create@gmail.com>
2019-07-16 19:51:12 +0530

    Initial Setup

---
+++ ios/Pods/Target Support Files/Pods-backgroundTimer-tvOSTests/Pods-backgroundTimer-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_backgroundTimer_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_backgroundTimer_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-backgroundTimer/Pods-backgroundTimer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_backgroundTimer : NSObject
+@end
+@implementation PodsDummy_Pods_backgroundTimer
+@end
